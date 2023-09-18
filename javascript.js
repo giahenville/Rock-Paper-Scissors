e8f65b47ff7c5402527839b76a1d7622d35b0e96
Gia Henville <140769584+giabui@users.noreply.github.com>
2023-09-18 15:16:05 -0400

    feat: Add function for computer to randomly select rock, paper, or scissors
    Init getComputerChoice function in order for the computer to randomly select from an array.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
 // Ask user input (choose between rock, paper, or scissors)
 // Computer randomly selects rock, paper or scissors
+function getComputerChoice(){
+    options = ["rock", "paper", "scissors"];
+    console.log(options[ (Math.floor(Math.random() * options.length))]);
+    return;
+}
+getComputerChoice();
 // Compare both selections
     // rock beats scissors
     // rock loses to paper 
