5cee70855768e7d66923e8e30a15fa1176f7222e
Gia Henville <140769584+giabui@users.noreply.github.com>
2023-09-18 13:59:16 -0400

    docs: Add pseudocode to javascript.js

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// Ask user input (choose between rock, paper, or scissors)
+// Computer randomly selects rock, paper or scissors
+// Compare both selections
+    // rock beats scissors
+    // rock loses to paper 
+    // paper beats rock
+    // paper loses to scissors
+    // scissors beats paper
+    // scissors loses to rock
