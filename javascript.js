68e4ec1d6e7a2d98986d010e8c3edf31710677d8
Gia Henville <140769584+giabui@users.noreply.github.com>
2023-09-18 20:58:54 -0400

    fix: Create variable for computer options array
    Add "let" to variable name so that computer recognizes it. Now it returns computer choice consistently.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,30 @@
 // Ask user input (choose between rock, paper, or scissors)
+
 // Computer randomly selects rock, paper or scissors
 function getComputerChoice(){
-    options = ["rock", "paper", "scissors"];
-    console.log(options[ (Math.floor(Math.random() * options.length))]);
-    return;
+    let options = ["rock", "paper", "scissors"];
+    return options[ (Math.floor(Math.random() * options.length -1)) ]; 
+    
+   
 }
-getComputerChoice();
+// getComputerChoice();
+
 // Compare both selections
+// let computerSelection = getComputerChoice();
+function firstRound(){
+    let playerSelection = prompt("Choose rock, paper, or scissors: ");
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    // tie
+    if (playerSelection.toLowerCase() === computerSelection){
+        return;
+    }
+    
     // rock beats scissors
     // rock loses to paper 
     // paper beats rock
     // paper loses to scissors
     // scissors beats paper
     // scissors loses to rock
+}
+firstRound();
