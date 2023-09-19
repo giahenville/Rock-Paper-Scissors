9a6c7558564c54a287a7af4fe93184621c25ad23
Gia Henville <140769584+giabui@users.noreply.github.com>
2023-09-19 17:31:29 -0400

    feat: Add game() function
    Create game() function so that player plays 5 rounds and the final score is returned

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-// Ask user input (choose between rock, paper, or scissors)
-
 // Computer randomly selects rock, paper or scissors
 function getComputerChoice(){
     let options = ["rock", "paper", "scissors"];
@@ -7,31 +5,44 @@ function getComputerChoice(){
 }
 
 // Compare both selections
-function firstRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    //in case of both players choosing the same choice 
-    if (playerSelection === computerSelection){
-        return "Draw";
-     // Player beats computer
-    }else if (playerSelection === "rock" && computerSelection === "scissors" ||
-     playerSelection === "scissors" && computerSelection == "paper" || 
-     playerSelection === "paper" && computerSelection === "rock" ){
-        return "You win! " + playerSelection + " beats " + computerSelection + "!";
-    }else{
-        return "You lose! " + computerSelection + " beats " + playerSelection + "!";
+function game(){
+
+    let score = 0;
+  
+    function firstRound(playerSelection, computerSelection){
+        //in case of both players choosing the same choice 
+        if (playerSelection === computerSelection){
+            let result = "Draw";
+            return "Your score is: " + score + " " + result;
+        // Player beats computer
+        }else if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "scissors" && computerSelection == "paper" || 
+        playerSelection === "paper" && computerSelection === "rock" ){
+            score += 1;
+            result = "You win! " + playerSelection + " beats " + computerSelection + "!";
+            return "Your score is: " + score + " " + result;
+        //Computer beats player
+        }else{
+            result =  "You lose! " + computerSelection + " beats " + playerSelection + "!";
+            return "Your score is: " + score + " " + result;
+        }   
+    }
+
+    for (let i = 1; i <= 5; i++){
+        let computerSelection = getComputerChoice();
+        // Ask user input (choose between rock, paper, or scissors)
+        let playerSelection = prompt("Choose rock, paper, or scissors:").toLowerCase(); 
+        console.log(firstRound(playerSelection, computerSelection));
     }
     
+    const finalResult = score >= 3
+        ? "Winner! Your final score is " + score
+        : "Losser! Your final score is " + score;
+    
+    return finalResult;
 }
+        
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-
-console.log(firstRound(playerSelection, computerSelection));
+console.log(game());
     
     
-    // rock loses to paper 
-    // paper beats rock
-    // paper loses to scissors
-    // scissors beats paper
-    // scissors loses to rock
-
